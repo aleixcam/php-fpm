62d43c1b7d7fcdbf4db04dc12c2a4c5d42fa25c8
Aleix Caminal <aleixcam96@gmail.com>
2021-03-25 17:58:10 +0100

    fix smol error

--- 7.4/entrypoint.sh
+++ 7.4/entrypoint.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-if [ ${PHP_XDEBUG_ENABLED} -ne 1 ]; then
+if [ "$PHP_XDEBUG_ENABLED" = "1" ]; then
 	cd /usr/local/etc/php/ && mkdir -p disabled/ && mv conf.d/docker-php-ext-xdebug.ini disabled/ || :
 fi
+
+cd /var/www
 
 exec "$@"
